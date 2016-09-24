afb67b69f6fc636cf861897b6c4e42609ac598ce
Ankur Khemani <ankurkhemani139@gmail.com>
2016-09-24 19:07:02 -0400

    first commit

---
+++ app/src/main/java/ankur/hackgt/deepwatch/Config.java
@@ -0,0 +1,14 @@
+package ankur.hackgt.deepwatch;
+
+/**
+ * Created by Ankur on 24/09/16.
+ */
+public class Config {
+
+    // Google Console APIs developer key
+    public static final String DEVELOPER_KEY = "AIzaSyCyhP6js3ZEK5ALOG1PPnIQcs5wXP6cu4Y";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "S0YmnV5i1fA";
+}
+
