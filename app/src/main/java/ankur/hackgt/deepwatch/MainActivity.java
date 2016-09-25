012ae3c5686b2030466b2916bc331b25bca6d60b
Ankur Khemani <ankurkhemani139@gmail.com>
2016-09-24 23:01:39 -0400

    added textureview

--- app/src/main/java/ankur/hackgt/deepwatch/MainActivity.java
+++ app/src/main/java/ankur/hackgt/deepwatch/MainActivity.java
@@ -1,28 +1,34 @@
 package ankur.hackgt.deepwatch;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
-import android.content.Intent;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
+import android.widget.ImageView;
+import android.widget.MediaController;
 
 import com.microsoft.projectoxford.face.*;
 import com.microsoft.projectoxford.face.contract.*;
 
+import java.io.IOException;
 
-public class MainActivity extends YouTubeBaseActivity implements
-        YouTubePlayer.OnInitializedListener {
 
-    private static final int RECOVERY_DIALOG_REQUEST = 1;
-    // YouTube player view
-    private YouTubePlayerView youTubeView;
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+
+
+    private static final String TAG = MainActivity.class.getName();
+    private MediaPlayer mMediaPlayer;
+    private TextureView mPreview;
+
     Button deepWatch;
     private FaceServiceClient faceServiceClient;
 
@@ -36,10 +42,8 @@ public class MainActivity extends YouTubeBaseActivity implements
 
         setContentView(R.layout.activity_main);
 
-        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-
-        // Initializing video player with developer key
-        youTubeView.initialize(Config.GOOGLE_DEVELOPER_KEY, this);
+        mPreview = (TextureView) findViewById(R.id.textureView);
+        mPreview.setSurfaceTextureListener(this);
 
 
         faceServiceClient = new FaceServiceRestClient(Config.MICROSOFT_DEVELOPER_KEY);
@@ -48,47 +52,76 @@ public class MainActivity extends YouTubeBaseActivity implements
         deepWatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.d("bitmap" , "" + getBitmap());
+                ImageView im = (ImageView) findViewById(R.id.image);
+                im.setImageBitmap(getBitmap());
             }
         });
 
     }
 
-    @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider,
-                                        YouTubeInitializationResult errorReason) {
-        if (errorReason.isUserRecoverableError()) {
-            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
-        } else {
-            String errorMessage = String.format(
-                    getString(R.string.error_player), errorReason.toString());
-            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+    public Bitmap getBitmap(){
+        return  mPreview.getBitmap();
+    }
+
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+        Surface surface = new Surface(surfaceTexture);
+
+        try {
+            mMediaPlayer = new MediaPlayer();
+            String FILE_NAME = "android.resource://" + getPackageName() + "/" + R.raw.video_1;
+            mMediaPlayer
+                    .setDataSource(this, Uri.parse(FILE_NAME));
+            mMediaPlayer.setSurface(surface);
+            mMediaPlayer.setLooping(true);
+
+            // don't forget to call MediaPlayer.prepareAsync() method when you use constructor for
+            // creating MediaPlayer
+            mMediaPlayer.prepareAsync();
+            // Play video when the media source is ready for playback.
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (SecurityException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (IllegalStateException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, e.getMessage());
         }
     }
 
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider,
-                                        YouTubePlayer player, boolean wasRestored) {
-        if (!wasRestored) {
-
-            // loadVideo() will auto play video
-            // Use cueVideo() method, if you don't want to play it automatically
-            player.loadVideo(Config.YOUTUBE_VIDEO_CODE);
-
-            // Hiding player controls
-            //player.setPlayerStyle(PlayerStyle.CHROMELESS);
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mMediaPlayer != null) {
+            // Make sure we stop video and release resources when activity is destroyed.
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RECOVERY_DIALOG_REQUEST) {
-            // Retry initialization if user performed a recovery action
-            getYouTubePlayerProvider().initialize(Config.DEVELOPER_KEY, this);
-        }
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i2) {
+
     }
 
-    private YouTubePlayer.Provider getYouTubePlayerProvider() {
-        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        return false;
     }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+    }
+
+
 }
