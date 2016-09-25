e1ee5669f376a26f5bc1cda0126a150f472d32d8
Minaxi Singla <Minaxi Singla>
2016-09-24 20:05:02 -0700

    added face recognition

--- app/src/main/java/ankur/hackgt/deepwatch/MainActivity.java
+++ app/src/main/java/ankur/hackgt/deepwatch/MainActivity.java
@@ -1,21 +1,35 @@
 package ankur.hackgt.deepwatch;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Region;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Face;
+import com.microsoft.projectoxford.face.contract.FaceRectangle;
 
-import com.microsoft.projectoxford.face.*;
-import com.microsoft.projectoxford.face.contract.*;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends YouTubeBaseActivity implements
         YouTubePlayer.OnInitializedListener {
@@ -48,10 +62,21 @@ public class MainActivity extends YouTubeBaseActivity implements
         deepWatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String url = "http://television.mxdwn.com/wp-content/uploads/2014/07/635127683823200000.jpg";
+                try {
+                    Object o = new URL(url).getContent();
+                    Log.w("I got a ", o.getClass().getName());
+                    InputStream inputStream = (InputStream) new URL(url).getContent();
+                    detectAndFrame(inputStream);
+                }
+                catch (MalformedURLException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
-
     }
 
     @Override
@@ -84,11 +109,71 @@ public class MainActivity extends YouTubeBaseActivity implements
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RECOVERY_DIALOG_REQUEST) {
             // Retry initialization if user performed a recovery action
-            getYouTubePlayerProvider().initialize(Config.DEVELOPER_KEY, this);
+            getYouTubePlayerProvider().initialize(Config.GOOGLE_DEVELOPER_KEY, this);
         }
     }
 
     private YouTubePlayer.Provider getYouTubePlayerProvider() {
         return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+
+    private void detectAndFrame(final InputStream inputStream){
+
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>() {
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Detecting...");
+                            Face[] result = faceServiceClient.detect(
+                                    params[0],
+                                    true,         // returnFaceId
+                                    false,        // returnFaceLandmarks
+                                    null           // returnFaceAttributes: a string like "age, gender"
+                            );
+                            if (result == null) {
+                                publishProgress("Detection Finished. Nothing detected");
+                                return null;
+                            }
+                            publishProgress(
+                                    String.format("Detection Finished. %d face(s) detected",
+                                            result.length));
+                            return result;
+                        } catch (Exception e) {
+                            publishProgress("Detection failed");
+                            return null;
+                        }
+                    }
+
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+
+                        if (result == null) return;
+                        Bitmap bmp = BitmapFactory.decodeStream(inputStream);
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                        imageView.setImageBitmap(getFaceRectanglesOnBitmap(bmp, result).get(0));
+                        bmp.recycle();
+                    }
+                };
+    }
+
+    private static List<Bitmap> getFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+        List<Bitmap> result = new ArrayList<Bitmap>();
+
+        if (faces != null) {
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                Bitmap bitmap = Bitmap.createBitmap(faceRectangle.width, faceRectangle.height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas();
+                canvas.clipRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        Region.Op.REPLACE);
+                result.add(bitmap);
+            }
+        }
+        return result;
     }
 }
